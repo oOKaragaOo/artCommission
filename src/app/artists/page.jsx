d3c4fe571c5bae7c3236182415822f44d41d46d1
Thanat <tk.2546thanat.1@gmail.com>
2025-04-23 00:12:23 +0700

    Split Css Explore

--- src/app/artists/page.jsx
+++ src/app/artists/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import styles from '../../styles/Home.module.css';
+import styles from '../../styles/artists.module.css';
 import Navbarone from '../components/Navbarone';
 import Sidebar from '../components/Sidebar';
 
